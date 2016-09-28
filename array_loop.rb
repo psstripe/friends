99cdf7f767326b5e3d415cc68861171b1386a888
Pamela Shen <pamela@stripe.com>
2016-09-27 20:06:03 -0700

    adding in array_loop.rb and updating data_types.rb

---
+++ array_loop.rb
@@ -0,0 +1,4 @@
+good_friends = ["katie", "rahel", "alexandra"]
+good_friends.each do |name|
+  puts "#{name} is a good friend"
+end
---
+++ data_types.rb
@@ -0,0 +1,12 @@
+h = {"hash?" => "yep, it\'s a hash!", "the answer to everything" => 42, :linux => "fun for coders."}
+puts "Stringy string McString!".class
+puts 1.class
+puts 1.class.superclass
+puts 1.class.superclass.superclass
+puts 4.3.class
+puts 4.3.class.superclass
+puts nil.class
+puts h.class
+puts :symbol.class
+puts [].class
+puts (1..8).class
